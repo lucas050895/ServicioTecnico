f2356829f9a64f20788c86ee7e542442d3c5e2b5
Lucas Conde <lucasconde742@gmail.com>
2024-06-13 23:33:20 -0300

    Nueva logica recibir los datos e insertarlos en BD

--- links/subir.php
+++ links/subir.php
@@ -1,26 +1,69 @@
 <?php
     include('../bd/conecxion.php');
 
-    $nombre = $_POST['nombre'];
-    $celular = $_POST['celular'];
-    $direccion = $_POST['direccion'];
-    $tipo = $_POST['tipo'];
-    $modelo = $_POST['modelo'];
-    $problema = $_POST['problema'];
+    $nombre         =   ucwords($_POST['nombre']);  // Pasar a mayúsculas solo la primera letra
+    $celular        =   $_POST['celular'];
+    $direccion      =   trim(strtoupper($_POST['direccion'])); // Pasar a mayuscula
+    $tipo           =   $_POST['tipo'];
+    $modelo         =   $_POST['modelo'];
+    $problema       =   $_POST['problema'];
 
-    $file_name = $_FILES['uploadedFile']['name'];
-    $file_tmp = $_FILES['uploadedFile']['tmp_name'];
-    $route = "../public/img/". $file_name;
+    //Verificando si existe el directorio
+    $dirLocal = "../img";
 
-    move_uploaded_file($file_tmp, $route);
+    if (!file_exists($dirLocal)) {
+        mkdir($dirLocal, 0777, true);
+    }
 
-
-    $insert = "INSERT INTO datos(nombre,celular,direccion,maquina,modelo,revisar,foto1)
-            VALUES('$nombre','$celular','$direccion','$tipo','$modelo','$problema', '$file_name')";
-        
-    $sql_query = mysqli_query($conexion, $insert)
+    $miDir         = opendir($dirLocal); //Habro el directorio
 
 
+    if(isset($_POST['submit']) && count($_FILES['fotos_maquina']['name'])>0){
+
+        // Recorrer cada archivo subido
+        foreach ($_FILES['fotos_maquina']['name'] as $i => $name) {
+
+            //strlen método de php pues devuelve la longitud de una cadena
+            if (strlen($_FILES['fotos_maquina']['name'][$i]) > 1) {
+            
+            $fileName          = $_FILES['fotos_maquina']['name'][$i];
+            $sourceFoto        = $_FILES['fotos_maquina']['tmp_name'][$i];
+            $tamanoFoto        = $_FILES["fotos_maquina"]['size'][$i];
+            $restricciontamano = "500";//MB
+
+                if((($tamanoFoto/1024)/1024)<=$restricciontamano){
+
+                /**Renombrando cada foto que llega desde el formulario */
+                $nuevoNombreFile    = substr(md5(uniqid(rand())),0,15);
+                $extension_foto     = pathinfo($fileName, PATHINFO_EXTENSION);
+                $nombreFoto         = $nuevoNombreFile.'_'.$nombre.'.'.$extension_foto;
+                $resultadoFotos     = $dirLocal.'/'.$nombreFoto;
+
+                    // Mover archivo a una ubicación permanente
+                    move_uploaded_file($sourceFoto, $resultadoFotos);
+                
+                    // Insertar información del archivo en la base de datos
+                    $sql = "INSERT INTO fotos_clientes (foto, nombre_cliente) VALUES ('{$nombreFoto}', '{$nombre}')";
+                    mysqli_query($conexion, $sql);
+                    
+                }else{
+                    echo'<p style="color:red">Existe una foto que supera el peso Maximo de '.$tamanoFoto.'</p>';
+                }
+            }
+        }
+
+
+        $sql = "INSERT INTO clientes (nombre, celular, direccion, modelo, tipo, problema) VALUES ('{$nombre}', '{$celular}', '{$direccion}', '{$modelo}', '{$tipo}', '{$problema}')";
+        mysqli_query($conexion, $sql);
+
+
+    }
+
+    // Cerrar conexión a la base de datos
+    mysqli_close($conexion);
+
+    // Redirigir a la página de inicio
+    // header("Location: ../index.php");
 ?>
 
 <!DOCTYPE html>
@@ -45,11 +88,24 @@
 </head>
 <body>
     <main>
-        <h1>Se subio correctamente el cliente.</h1>
 
         <div>
-            <button><a href="links/agregar.php">Agregar clientes</a></button>
-            <button><a href="links/ver.php">Ver clientes</a></button>
+            <h1>Se ha subido <br>correctamente</br> el cliente.</h1>
+        </div>
+        
+        <div>
+            <button>
+                <a href="agregar.php">
+                    <i class="fas fa-plus"></i>
+                    Agregar
+                </a>
+            </button>
+            <button>
+                <a href="ver.php">
+                    <i class="fas fa-eye"></i>
+                    Ver
+                </a>
+            </button>
         </div>
     </main>
 </body>
