0fac36b6d963d87bdafcb59c809c839ddbcf69a1
Lucas Conde <lucasconde742@gmail.com>
2024-06-13 23:26:30 -0300

    Nueva estructura e iconos

--- index.php
+++ index.php
@@ -21,11 +21,24 @@
 <body>
 <body>
     <main>
-        <h1>Carga de Clientes</h1>
+        <div>
+            <i class="fas fa-user-alt"></i>
+            <h1>Carga de Clientes</h1>
+        </div>
         
         <div>
-            <button><a href="links/agregar.php">Agregar<br>Clientes</a></button>
-            <button><a href="links/ver.php">Ver<br>Clientes</a></button>
+            <button>
+                <a href="links/agregar.php">
+                    <i class="fas fa-plus"></i>
+                    Agregar
+                </a>
+            </button>
+            <button>
+                <a href="links/ver.php">
+                    <i class="fas fa-eye"></i>
+                    Ver
+                </a>
+            </button>
         </div>
 
     </main>
