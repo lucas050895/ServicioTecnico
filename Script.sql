0560a30bef72ab8427194957f188a3556d7a197e
Lucas Conde <lucasconde742@gmail.com>
2024-06-23 14:00:51 -0300

    Nuevo script con datos de ejemplo

--- Script.sql
+++ Script.sql
@@ -1,35 +1,140 @@
-CREATE DATABASE servicio;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-06-2024 a las 18:48:56
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-USE servicio;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `servicio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
 
 CREATE TABLE `clientes` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `nombre` VARCHAR(45) DEFAULT NULL,
-  `celular` INT(15) NOT NULL,
-  `direccion` VARCHAR(50) NOT NULL,
-  `modelo` VARCHAR(50) NOT NULL,
-  `tipo` VARCHAR(50) NOT NULL,
-  `problema` VARCHAR(20) NOT NULL,
-  `fecha` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `celular` int(15) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `problema` varchar(20) NOT NULL,
+  `fecha` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `celular`, `direccion`, `modelo`, `tipo`, `problema`, `fecha`) VALUES
+(1, 'Lucas Conde', 123456789, 'CALLE FALSA 123', 'Asus', 'Computadora', 'Resivsar1', '2024-06-21 23:00:35');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotos_clientes`
+--
 
 CREATE TABLE `fotos_clientes` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `foto` VARCHAR(45) DEFAULT NULL,
-  `nombre_cliente` VARCHAR(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL,
+  `foto` varchar(45) DEFAULT NULL,
+  `nombre_cliente` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE problema(
-	id_problema INT PRIMARY KEY AUTO_INCREMENT,
-  descripcion VARCHAR(20)
-);
+--
+-- Volcado de datos para la tabla `fotos_clientes`
+--
 
-INSERT INTO problema(descripcion) 
-VALUE
-("Resivsar1"),
-("Resivsar2"),
-("Resivsar3"),
-("Resivsar4"),
-("Resivsar5");
+INSERT INTO `fotos_clientes` (`id`, `foto`, `nombre_cliente`) VALUES
+(1, '0fd6c1639dd9d22_Lucas Conde.jpg', 'Lucas Conde'),
+(2, '0e361a6a579e3bc_Lucas Conde.png', 'Lucas Conde'),
+(3, 'c003fdf647f50c0_Lucas Conde.jpeg', 'Lucas Conde'),
+(4, '3f74faa53c1d97a_Lucas Conde.jpg', 'Lucas Conde'),
+(5, '1186119b3ee776b_Lucas Conde.jpg', 'Lucas Conde');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `problema`
+--
+
+CREATE TABLE `problema` (
+  `id_problema` int(11) NOT NULL,
+  `descripcion` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `problema`
+--
+
+INSERT INTO `problema` (`id_problema`, `descripcion`) VALUES
+(1, 'Revisar1'),
+(2, 'Revisar2'),
+(3, 'Revisar3'),
+(4, 'Revisar4'),
+(5, 'Revisar5');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `fotos_clientes`
+--
+ALTER TABLE `fotos_clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `problema`
+--
+ALTER TABLE `problema`
+  ADD PRIMARY KEY (`id_problema`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `fotos_clientes`
+--
+ALTER TABLE `fotos_clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `problema`
+--
+ALTER TABLE `problema`
+  MODIFY `id_problema` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
